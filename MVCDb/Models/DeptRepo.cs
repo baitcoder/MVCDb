1ef251f33a03c6008ccbd103268b9f1870c0982e
Baitcoder <baitcoder@gmail.com>
2022-08-16 22:36:25 +0530

    Add project files.

---
+++ MVCDb/Models/DeptRepo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MVCDb.Models
+{
+    public class DeptRepo : IDept
+    {
+        DB1045Context db=new DB1045Context();
+        public List<Dept> GetAllDepts()
+        {
+            var data = db.Depts.ToList();
+            return data;
+        }
+        public void AddDept(Dept dept)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteDept(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditDept(Dept dept)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        public Dept GetDeptById(int id)
+        {
+            var data=db.Depts.Find(id);
+            return data;
+        }
+    }
+}
---
+++ MVCDb/Models/IDept.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MVCDb.Models
+{
+    public interface IDept
+    {
+        List<Dept> GetAllDepts();
+        Dept GetDeptById(int id);
+        void DeleteDept(int id);
+        void EditDept(Dept dept);
+        void AddDept(Dept dept);
+    }
+}
